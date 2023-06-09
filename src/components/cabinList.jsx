e3e7708b24941074bc74fd1c064641d2c19da5c3
hulff <ph2717693@gmail.com>
2023-06-09 10:39:01 -0300

    improve cabinList

--- src/components/cabinList.jsx
+++ src/components/cabinList.jsx
@@ -1,14 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { BsSnow } from "react-icons/bs";
 import { TbWindow, TbWindowOff } from "react-icons/tb";
+import { MdOutlineRestartAlt } from "react-icons/md";
+import { CgRemove, CgLogOff, CgInfo } from "react-icons/cg";
 import { FaWifi } from "react-icons/fa";
 import Button from "./button";
 import CabinsManage from "./cabinsManage";
 import "./styles/cabinList.css";
-import { writeFullCabinsData } from "../services/firebase";
+import { writeFullCabinsData, getCabinListData } from "../services/firebase";
 
 const CabinList = ({ code, cabinsList, cabinsData, setCabinsData }) => {
+  const divCabin = useRef(null);
   function activeAll(e) {
     console.log(code);
     console.log(e);
@@ -57,44 +60,77 @@ const CabinList = ({ code, cabinsList, cabinsData, setCabinsData }) => {
       writeFullCabinsData(code, newCabinsData);
     }
   }
+  function sincronize() {
+    let sincronizeBtn = document.getElementById("sincronizeBtn");
+    divCabin.current = document.getElementById("cabinListDiv");
+    sincronizeBtn.children[0].style.animation = "spin 1s infinite linear";
+    divCabin.current.style.pointerEvents = "none";
+    divCabin.current.style.filter = "brightness(0.5)";
+
+    const getCabinsData = async () => {
+      try {
+        const data = await getCabinListData(code);
+        console.log(data);
+        setCabinsData(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        //retirar o login
+        sincronizeBtn.children[0].style.animation = "";
+        divCabin.current.style.pointerEvents = "";
+        divCabin.current.style.filter = "brightness(1)";
+      }
+    };
+    getCabinsData();
+  }
   return (
     <>
       <div className="container-list">
-        <h1>Lista de cabines:</h1>
-        <h2>Atalhos rápidos:</h2>
-        <ul>
-          <li>
-            <Button dataInfo={true} func={activeAll} name={"ac"}>
-              <p>Marcar todos</p> <BsSnow />
-            </Button>
-          </li>
-          <li>
-            <Button dataInfo={false} func={activeAll} name={"ac"}>
-              <p>Limpar todos</p> <BsSnow />
-            </Button>
-          </li>
-          <li>
-            <Button dataInfo={true} func={activeAll} name={"window"}>
-              <p>Marcar todos</p> <TbWindow />
-            </Button>
-          </li>
-          <li>
-            <Button dataInfo={false} func={activeAll} name={"window"}>
-              <p>Limpar todos</p> <TbWindow />
-            </Button>
-          </li>
-          <li>
-            <Button dataInfo={true} func={activeAll} name={"wifi"}>
-              <p>Marcar todos</p> <FaWifi />
-            </Button>
-          </li>
-          <li>
-            <Button dataInfo={false} func={activeAll} name={"wifi"}>
-              <p>Limpar todos</p> <FaWifi />
-            </Button>
-          </li>
-        </ul>
+        <h1>Lista de cabines</h1>
         <div>
+          <ul>
+            <li>
+              <h2>Atalhos rápidos</h2>
+            </li>
+            <li>
+              <Button dataInfo={true} func={activeAll} name={"ac"}>
+                <CgRemove />
+              </Button>
+              <Button dataInfo={false} func={activeAll} name={"ac"}>
+                <CgLogOff />
+              </Button>
+              <Button>
+                <BsSnow />
+              </Button>
+            </li>
+            <li>
+              <Button dataInfo={true} func={activeAll} name={"window"}>
+                <CgRemove />
+              </Button>
+              <Button dataInfo={false} func={activeAll} name={"window"}>
+                <CgLogOff />
+              </Button>
+              <Button>
+                <TbWindow />
+              </Button>
+            </li>
+            <li>
+              <Button dataInfo={true} func={activeAll} name={"wifi"}>
+                <CgRemove />
+              </Button>
+              <Button dataInfo={false} func={activeAll} name={"wifi"}>
+                <CgLogOff />
+              </Button>
+              <Button>
+                <FaWifi />
+              </Button>
+            </li>
+          </ul>
+          <Button id="sincronizeBtn" func={sincronize}>
+            <MdOutlineRestartAlt></MdOutlineRestartAlt>
+          </Button>
+        </div>
+        <div id="cabinListDiv">
           <CabinsManage
             setCabinsData={setCabinsData}
             code={code}
